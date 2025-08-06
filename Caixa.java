fe6dcebef5fd0bb04e7aa210ecf0b130e4019e45
Luizmiguel1784 <santosalmeidaluizmiguel28@gmail.com>
2025-08-06 11:06:01 -0300

    Add files via upload

---
+++ Caixa.java
@@ -0,0 +1,12 @@
+package aula3;
+
+public class Caixa {
+
+	
+	String nome;
+	int preco;
+	double desconto;
+	
+	
+	
+}
---
+++ Main.java
@@ -0,0 +1,23 @@
+package aula3;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		Pessoa pe = new Pessoa("nome", 0);
+		
+		System.out.printf("Qual seu nome \nQual sua idade \n");
+		pe.setNome(sc.next());
+		pe.setIdade(sc.nextInt());;
+		
+		if()
+		
+		
+		sc.close();
+	}
+
+}
