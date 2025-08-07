b2167949027d08094d71253780a17704eda217a5
Luizmiguel1784 <santosalmeidaluizmiguel28@gmail.com>
2025-08-07 11:03:11 -0300

    Caixa.java

--- Caixa.java
+++ Caixa.java
@@ -2,11 +2,73 @@ package aula3;
 
 public class Caixa {
 
+		private String c = "celular";
+	private int vc = 1700;
+	double  des, valor;
 	
-	String nome;
-	int preco;
-	double desconto;
+	
+
+
+
+
+
+	public String getC() {
+		return c;
+	}
+
+
+
+
+
+
+
+	public void setC(String c) {
+		this.c = c;
+	}
+
+
+
+
+
+
+
+	public int getVc() {
+		return vc;
+	}
+
+
+
+
+
+
+
+	public void setVc(int vc) {
+		this.vc = vc;
+	}
+
+
+
+
+
+
+
+	public void produto (double des) {
+		
+		if(des <100) {
+		des = (des / 100) *vc;
+		
+		
+		
+		System.out.println(vc - des);
+		}
+		
+		else {
+			System.out.println("esse desconto é invalido diga um valido");
+		}
+		}
+		
 	
 	
 	
 }
+
